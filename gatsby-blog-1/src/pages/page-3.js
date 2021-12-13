dcd82a829e2dfb1bc6803ba37362b02063f155d6
yarnJ <yarnJ33@gmail.com>
2021-12-13 04:44:05 -0800

    gatsby-blog-1

---
+++ gatsby-blog-1/src/pages/page-3.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>third page</h1>
+  </Layout>
+);
+
+export default ThirdPage;
